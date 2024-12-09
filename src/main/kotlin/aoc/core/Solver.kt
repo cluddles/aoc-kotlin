66a6480d00310f758a49e630af3f0ccc85d27456
Dan Fielding <dgfielding@gmail.com>
2024-12-09 16:50:42 +0000

    Harness shows elapsed time

--- src/main/kotlin/aoc/core/Solver.kt
+++ src/main/kotlin/aoc/core/Solver.kt
@@ -1,5 +1,8 @@
 package aoc.core
 
+import kotlin.time.measureTime
+import kotlin.time.measureTimedValue
+
 interface Solver<I, O> {
 
     /** Load input from [path] */
@@ -16,9 +19,21 @@ object Harness {
 
     /** Run [solver] for both parts of a day's puzzle, using input loaded from [path] */
     fun <I, O> run(solver: Solver<I, O>, path: String) {
-        val input = solver.prepareInput(path)
-        println(solver.solvePart1(input))
-        println(solver.solvePart2(input))
+        measureTime {
+            val (input, t) = measureTimedValue { solver.prepareInput(path) }
+            println("Loaded input in $t")
+
+            println("\nPart 1:")
+            measureTime {
+                println(solver.solvePart1(input))
+            }.also { println("Took $it") }
+
+            println("\nPart 2:")
+            measureTime {
+                println(solver.solvePart2(input))
+            }.also { println("Took $it") }
+
+        }.also { println("\nTotal time elapsed: $it") }
     }
 
 }
