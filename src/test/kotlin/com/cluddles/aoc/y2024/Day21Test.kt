4a46e5e62f88ff99348cfe662940a5620b2d3e73
Dan Fielding <dgfielding@gmail.com>
2024-12-23 15:38:12 +0000

    Day 21
    Part 2 took an embarrassing amount of time due to a bug with the
    directional pad pathing where `<` to `A` would always step over the gap
    
    I'm sure this could be nicer but I'm tired of looking at it

---
+++ src/test/kotlin/com/cluddles/aoc/y2024/Day21Test.kt
@@ -0,0 +1,20 @@
+package com.cluddles.aoc.y2024
+
+import com.cluddles.aoc.core.SolverInput
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day21Test {
+
+    val solver = Day21
+
+    val input = solver.prepareInput(SolverInput.fromPath(solver.examplePath))
+
+    @Test fun part1() {
+        assertThat(solver.solvePart1(input)).isEqualTo(126384)
+    }
+
+
+    // No example for part2
+
+}
